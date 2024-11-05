0dccb7b4ba86f4fc2df7f66d7cc57fcf943fe8fd
mohitjangra117@gmail.com <mohitjangra117@gmail.com>
2024-11-05 18:26:38 +0530

    Added js practice

---
+++ Practice Js/p1.js
@@ -0,0 +1,45 @@
+console.log("Hello");
+name="ABCD"
+console.log(typeof(name));
+let a=5;
+console.log(`The value of a is : ${a}`);
+// let b=prompt("Enter the value of b");
+// console.log(b);
+let msg = "    Hello    ";
+console.log(msg);
+let msg1= msg.trim();
+console.log(msg1);
+
+let str="Ilovecoding";
+console.log(str);
+str.replace("love","do");
+console.log(str);
+console.log(str.replace("love","do"));
+
+let num=[1,2,3,4,5,6,7,8];
+console.log(num.slice(-2,-1));
+
+
+let  num2=[11,12,13];
+num.splice(5,0,55,66);
+console.log(num);
+console.log(num.sort());
+let a1=[1,2];
+let a2=[1,2];
+let a3=a1;
+
+console.log(a1==a2);
+console.log(a1==a3);
+
+arr5=[[1,2],[3,4]];
+console.log(arr5[0][1]);
+
+let  aa1=["Apple", "banana","orange","pineapple"];
+
+for (fruit of aa1){
+    console.log(fruit);
+}
+
+for(char1 of "helloworld"){
+    console.log(char1);
+}
